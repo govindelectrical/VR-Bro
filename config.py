ab467440d6f4aa830b47ac37c1ed2a0e7a53e6e7
PheonixDeathNote <153576791+PheonixDeathNote@users.noreply.github.com>
2023-12-20 10:13:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-API_ID = API_ID = 20463500
+API_ID = API_ID = 28590119
 
-API_HASH = os.environ.get("API_HASH", "5db9bc6439f7b43daa75689edf65b431")
+API_HASH = os.environ.get("API_HASH", "2494557bf21e6c5152f26070aa1a97c7")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6920803794:AAHlsryRx1vNQe7FgdlbbnhxO6o5aFKr430")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6944466708:AAGiS73zYtTMBDd9P5_vhARwoaxkiHs8UqQ")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6333211826))
+OWNER = int(os.environ.get("OWNER", 6462391456))
 
-LOG = -1002008774612
+LOG = -1002094784974
 
