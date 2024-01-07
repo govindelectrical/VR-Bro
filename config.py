3ab3d7e0331795e7942d79b266cf224b814dd45e
govindelectrical <151295395+govindelectrical@users.noreply.github.com>
2024-01-07 18:59:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-API_ID = API_ID = 28590119
+API_ID = API_ID = 21104697
 
-API_HASH = os.environ.get("API_HASH", "2494557bf21e6c5152f26070aa1a97c7")
+API_HASH = os.environ.get("API_HASH", "2bbb3dc6c94bac3e5fcc492e86475063")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6944466708:AAGiS73zYtTMBDd9P5_vhARwoaxkiHs8UqQ")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6578569065:AAGRFhrfgfzzdDn6JNAmsFIZUfYSiWSR8lI")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 6462391456))
+OWNER = int(os.environ.get("OWNER", 5003073626))
 
-LOG = -1002094784974
+LOG = -1001956100269
 
